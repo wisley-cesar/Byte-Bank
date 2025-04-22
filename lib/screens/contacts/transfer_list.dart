c2265af41a7b354c0883353e3d2bd76a756e6021
Wisley César <wisleycesar32@gmail.com>
2025-04-22 15:08:50 -0300

    FEAT: Criando a opção de transação no dashbord

--- lib/screens/contacts/transfer_list.dart
+++ lib/screens/contacts/transfer_list.dart
@@ -1,24 +1,25 @@
 import 'package:bytebank/screens/contacts/widget/my_button_contacts.dart';
 import 'package:bytebank/screens/contacts/widget/my_card_to_list.dart';
+import 'package:bytebank/utils/my_rotas.dart';
 import 'package:bytebank/widget/my_app_bar.dart';
 import 'package:flutter/material.dart';
 
-class ContactsList extends StatefulWidget {
-  const ContactsList({super.key});
+class TransferList extends StatefulWidget {
+  const TransferList({super.key});
 
   @override
-  State<ContactsList> createState() => _ContactsListState();
+  State<TransferList> createState() => _ContactsListState();
 }
 
-class _ContactsListState extends State<ContactsList> {
+class _ContactsListState extends State<TransferList> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: MyAppBar(title: 'Contatos'),
+      appBar: MyAppBar(title: 'List de transferências'),
       body: MyCardToList(),
       floatingActionButton: MyButtonContacts(
         onPressed: () async {
-          await Navigator.of(context).pushNamed('/contacts-form');
+          await Navigator.of(context).pushNamed(MyRotas.contactsForm);
           setState(() {});
         },
       ),
--- lib/utils/my_rotas.dart
+++ lib/utils/my_rotas.dart
@@ -1,5 +1,5 @@
 class MyRotas {
   static const String home = '/home';
-  static const String contactsList = '/contacts-list';
+  static const String transfer = '/transfer';
   static const String contactsForm = '/contacts-form';
 }
