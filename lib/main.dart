404b18da616d82de9e5cdf42c2a87ce6852a5160
Wisley César <wisleycesar32@gmail.com>
2025-04-24 17:05:07 -0300

    FEAT: Criando InterceptorContract para mandar a resposta e o status da requisição

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'package:bytebank/screens/bytebankApp.dart';
+import 'package:bytebank/service/http/webc_client.dart';
 import 'package:flutter/material.dart';
 
 void main() {
   runApp(const Bytebankapp());
+  findAll();
 }
